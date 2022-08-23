710e2f41edc43460fc4ff58e04b8be434f00704b
A.Kotinov <alexander777ck@gmail.com>
2022-08-23 12:15:20 +0300

    Fixed config.

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -6,23 +6,20 @@ import { Footer } from '../components/Footer';
 
 builder.init('9d45f4226f17495a9222c2b8507d4420');
 
-function PageTemplate({ data }) {
-  const models = data?.allBuilderModels;
-  const page = models.onePage?.content;
-
-  return <>
-    <BuilderComponent name="page" content={page}/>
-  </>;
-}
-
-export default PageTemplate;
-
+// get content using @builder.io/gatsby plugin
 export const pageQuery = graphql`
-  query ($path: String!) {
+  query($path: String!) {
     allBuilderModels {
-      onePage(target: { urlPath: $path }) {
+      page(target: { urlPath: $path }) {
         content
       }
     }
   }
 `;
+
+// pass content to BuilderComponent
+export default function PageTemplate ({ data }) {
+  return <BuilderComponent
+    content={data.allBuilderModels.page[0].content}
+  />
+}
